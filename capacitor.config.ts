c15dad53a713d279af6961afce28583aaf5372ec
CapitanBoomer <di.munoza@duocuc.cl>
2023-01-20 14:50:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Diego Muñoz prueba 2 tav',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
